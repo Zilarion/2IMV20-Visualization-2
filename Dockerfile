e7a713783421b1efd6082f41dbb6f5595495b290
Rik Coenders <rik.coenders@gmail.com>
2016-12-14 23:50:27 +0100

    theme

--- Dockerfile
+++ Dockerfile
@@ -1,15 +1,26 @@
 FROM centos:7
 
+RUN curl --silent --location https://rpm.nodesource.com/setup_6.x | bash -
+
+RUN yum -y install gcc-c++ make
 RUN yum -y install epel-release
-RUN yum -y install python-pip
+RUN yum -y install nodejs
+RUN yum -y install nginx
 RUN yum clean all
 
-RUN mkdir opt/vis
-WORKDIR /opt/vis
+RUN npm install -g gulp-cli
 
-ADD requirements.txt requirements.txt
-RUN pip install --upgrade pip
-RUN pip install --upgrade setuptools
-RUN pip install -r requirements.txt
+ARG WORKDIR /opt/vis
+RUN mkdir $WORKDIR
+ADD package.json $WORKDIR/package.json
+ADD gruntfile.js $WORKDIR/gruntfile.js
+ADD src $WORKDIR/src
+WORKDIR $WORKDIR
+
+ADD nginx.conf /etc/nginx/nginx.conf
+
+RUN npm install --unsafe-perm
+
+CMD /usr/sbin/nginx
 
 EXPOSE 80
--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,46 @@
+'use strict';
+
+var browserify = require('browserify');
+var bulkSass = require('gulp-sass-bulk-import');
+var clean = require('gulp-clean');
+var concat = require('gulp-concat');
 var gulp = require('gulp');
+var inject = require('gulp-inject');
 var sass = require('gulp-sass');
+var source = require('vinyl-source-stream')
+var sourcemaps = require('gulp-sourcemaps');
 
+// Remove all build files
+gulp.task('clean', function () {
+    return gulp.src('www/*/**', {read: false})
+        .pipe(clean());
+});
+
+// Compile js
+gulp.task("js", function () {
+    return browserify('src/js/app.js').bundle()
+        .pipe(source('app.js'))
+        .pipe(gulp.dest('www/'));
+});
+
+// Compile sass
 gulp.task('sass', function() {
-    gulp.src('sass/**/*.scss')
+    return gulp.src('src/scss/index.scss')
+        .pipe(bulkSass())
+        .pipe(sourcemaps.init())
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css/'))
+        .pipe(concat('style.css'))
+        .pipe(sourcemaps.write('sourcemaps/'))
+        .pipe(gulp.dest('www/'))
 });
 
-//Watch task
-gulp.task('default',function() {
-    gulp.watch('sass/**/*.scss',['sass']);
+// Copy html
+gulp.task('html', ['js', 'sass'], function() {
+    return gulp.src('src/**/*.html')
+        .pipe(inject(gulp.src(['www/**/*.js', 'www/**/*.css'], {read: false}), {ignorePath: 'www'}))
+        .pipe(gulp.dest('www/'))
 });
+
+gulp.task('build', ['js', 'sass', 'html'], function() {});
+
+gulp.task('default', ['clean', 'build'], function() {});
