b0cac07e799958ac2d71ac252e95f665baa718d4
Rik Coenders <rik.coenders@gmail.com>
2016-12-15 02:36:51 +0100

    filter component

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
 'use strict';
 
 require('material-design-lite');
+require('../../node_modules/getmdl-select/src/js/getmdl-select');
+
 var WorldMap = require('./cards/WorldMap.js');
 
 new WorldMap('#worldmap', {});
